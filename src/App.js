dea394958cc562a20d5a1843216303a1612cb36b
escttsalo <escttsalo@gmail.com>
2021-06-11 00:32:59 -0600

    stretch

--- src/App.js
+++ src/App.js
@@ -10,18 +10,31 @@ import ShoppingCart from './components/ShoppingCart';
 //Contexts
 import ProductContext from './contexts/ProductContext';
 import CartContext from './contexts/CartContext';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	// const [cart, setCart] = useState([]);
+	// const initialCart = [];
+	const [cart, setCart] = useLocalStorage("Cart",[])
 
 	const addItem = item => {
-		setCart([...cart, item])
+		//Slight problem when page refreshes and doesn't recognize the item in the cart already
+		if(!cart.includes(item)){
+			setCart([...cart, item])
+		}else {
+			alert("Item already exists in cart!")
+		}
 	};
+
+	const removeItem = id => {
+		const newCart = cart.filter(item => item.id !== id)
+		setCart(newCart)
+	}
 
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{ products, addItem }}>
+			<ProductContext.Provider value={{ products, addItem, removeItem }}>
 				<CartContext.Provider value={cart}>
 					<Navigation />
 
